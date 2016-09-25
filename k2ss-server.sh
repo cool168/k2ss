b4d1ad6f7285b5a7a70d0d1c6b9e94fbaf7d00c5
cool168 <cool168@users.noreply.github.com>
2016-09-25 08:44:54 +0800

    Update k2ss-server.sh

--- k2ss-server.sh
+++ k2ss-server.sh
@@ -8,7 +8,7 @@ fi
 
 ss-server -s 0.0.0.0 -p 9090 -m chacha20 -k $PWD1 -t 60 -u -A >/dev/null 2>&1 &
 sleep 5
-/app/kcp/server_linux_amd64 -l :39900 -t 127.0.0.1:9090 -mode fast2 -nocomp -mtu 1400 -sndwnd 2048 -rcvwnd 2048 -crypt salsa20 -key $PWD2 >/dev/null 2>&1
+/app/server_linux_amd64 -l :39900 -t 127.0.0.1:9090 -mode fast2 -nocomp -mtu 1400 -sndwnd 2048 -rcvwnd 2048 -crypt salsa20 -key $PWD2 >/dev/null 2>&1
 while [[ true ]]; do
 	sleep 1
 done
