80e9d153b0f71fb666cc2c66350312f140f70035
cool168 <cool168@users.noreply.github.com>
2016-10-11 17:51:18 +0800

    Update k2ss-server.sh

--- k2ss-server.sh
+++ k2ss-server.sh
@@ -6,9 +6,15 @@ if [ ! -f "$INSTALL_FLAG" ]; then
 	touch $INSTALL_FLAG
 fi
 
-ss-server -s 127.0.0.1 -p 9090 -m chacha20 -k ss$PWD1 -t 60 -u -A >/dev/null 2>&1 &
-sleep 5
-/app/server_linux_amd64 -l :39900 -t 127.0.0.1:9090 -mode fast2 -nocomp -mtu 1400 -sndwnd 2048 -rcvwnd 2048 -crypt salsa20 -key kk$PWD2 >/dev/null 2>&1
-while [[ true ]]; do
+if [ $ENABLE_HTTP = "no" ]
+then
+     ss-server -s 0.0.0.0 -p 9090 -m chacha20 -k ss$PWD1 -t 60 -u -A >/dev/null 2>&1
+else
+     ss-server -s 127.0.0.1 -p 9090 -m chacha20 -k ss$PWD1 -t 60 -u -A >/dev/null 2>&1 &
+     sleep 5
+     /app/server_linux_amd64 -l :39900 -t 127.0.0.1:9090 -mode fast2 -nocomp -mtu 1400 -sndwnd 2048 -rcvwnd 2048 -crypt salsa20 -key kk$PWD2 >/dev/null 2>&1
+     while [[ true ]]; do
 	sleep 1
-done
+     done
+fi
+
